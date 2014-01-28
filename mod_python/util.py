d5e32d5ae8be9cd7154fa74229638cf41a95895f
John Harrison <john@bloomonkey.co.uk>
2014-01-28 23:06:44 +0000

    improved support for `mod_python.utils.FieldStorage`

--- mod_python/util.py
+++ mod_python/util.py
@@ -1,25 +1,69 @@
 
+from cgi import FieldStorage as CGIFieldStorage
 from cgi import parse_qs as cgi_parse_qs, parse_qsl as cgi_parse_qsl
 
 
-class FieldStorage(object):
+class Field(object):
 
-    def __init__(self, req):
+    def __init__(self, name, value):
+        raise NotImplementedError()
+
+
+class StringField(str):
+
+    def __init__(self, value):
+        super(StringField, self).__init__(value)
+
+    @property
+    def value(self):
+        return self
+
+
+class FieldStorage(CGIFieldStorage):
+
+    def __init__(self, req, keep_blank_values=False, strict_parsing=None):
         """Constructor.
 
         :arg req: The request object
         :type req: mod_python_wsgi.request.ModPythonRequest
+        :arg keep_blank_values: blank values in URL encoded form data should
+            be treated as blank strings
+        :type keep_blank_values: bool
+
         """
         # Parse request parameters into a single data structure
         self._multidict = req.request.params
 
-    def __getattr__(self, name):
-        if name in ['get', 'getfirst', 'getvalue']:
-            return self._multidict.get
-        elif name == 'getlist':
-            return self._multidict.getall
+    def get(self, name, default=None):
+        val = self._multidict.get(name, default)
+        if hasattr(val, 'read'):
+            return Field(name, val)
+        elif val is not None:
+            return StringField(val)
+
+    def getlist(self, name):
+        l = []
+        for val in self._multidict.getall(name):
+            if hasattr(val, 'read'):
+                l.append(Field(name, val))
+            else:
+                l.append(StringField(val))
+        return l
+
+    def getfirst(self, name, default=None):
+        return self.get(name, default)
+
+    def getvalue(self, name, default=None):
+        val = self.getlist(name)
+        if not val:
+            return default
+        elif len(val) == 1:
+            return val[0]
         else:
-            return getattr(self._multidict, name)
+            return val
+
+    def has_key(self, key):
+        return key in self._multidict
 
 
 def parse_qs(*args):
