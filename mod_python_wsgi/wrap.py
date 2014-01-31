51416e5aef9465300144aa182c0c2a67fee16dd5
John Harrison <john.harrison@liv.ac.uk>
2014-01-31 12:49:13 +0000

    add WSGI compliant wrapper for Basic auth
    Add `wrap.BasicAuthModPythonWSGIApp`, a WSGI compliant wrapper for
    a mod_python handler callable and a mod_python authentication handler
    
        application = BasicAuthModPythonWSGIApp(handler, authenhandler)

--- mod_python_wsgi/wrap.py
+++ mod_python_wsgi/wrap.py
@@ -4,6 +4,7 @@ This is usually the `handler` function that would have been configured for
 mod_python.
 """
 
+from mod_python import apache
 from mod_python_wsgi.request import ModPythonRequest
 
 
@@ -31,3 +32,27 @@ class ModPythonWSGIApp(object):
         finally:
             if request.cleanup is not None:
                 request.cleanup(request.cleanupData)
+
+
+class BasicAuthModPythonWSGIApp(ModPythonWSGIApp):
+
+    def __init__(self, callable_, authcallable, realm="Protected"):
+        super(BasicAuthModPythonWSGIApp, self).__init__(callable_)
+        self.authcallable = authcallable
+        self.realm = realm
+
+    def __call__(self, environ, start_response):
+        request = ModPythonRequest(environ)
+        # Check authentication callable returns zero status
+        authstatus = self.authcallable(request)
+        if authstatus == 401:
+            # Add WWW-Authenticate field
+            request.response.status = "401 Unauthorized"
+            request.response.headers['WWW-Authenticate'] = (
+                'Basic realm="{0}"'.format(self.realm)
+            )
+        elif authstatus == apache.OK:
+            self.excecute_callable(request)
+        else:
+            request.response.status_code = authstatus
+        return request.response(environ, start_response)
