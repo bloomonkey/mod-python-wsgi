d03dcf9c84db5aa43ffcb62a5012892e00b29a6f
John Harrison <john.harrison@liv.ac.uk>
2014-02-03 14:12:21 +0000

    add rudimentary `mod_python.Cookie` module

--- mod_python/Cookie.py
+++ mod_python/Cookie.py
@@ -1,5 +1,9 @@
-'''
-Created on 28 Jan 2014
 
-@author: johnph
-'''
+class Cookie(object):
+    pass
+
+
+def get_cookies(req, cls=Cookie):
+    cookie_dict = req.request.cookies
+    cookie_dict.has_key = cookie_dict.__contains__
+    return cookie_dict
